8833d6ed6be401e6b6ea7c53a03701da6bdb655c
Diagne Axel <axel.diagne2021@campus-eni.fr>
2021-07-20 00:11:59 +0200

    Tp 1 Module 2 Utilisation de twig pour mes templates

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,15 +13,13 @@ class MainController extends AbstractController
      * @Route("/", name="main_home")
      */
     public function home(){
-        echo "Hi";
-        die();
+        return $this->render('main/home.html.twig');
     }
 
     /**
      * @Route("/test", name="main_test")
      */
     public function test(){
-        echo "test";
-        die();
+        return $this->render('main/test.html.twig');
     }
 }
