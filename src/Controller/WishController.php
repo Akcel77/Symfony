8c5b227e8dfd9f7bcd443cec4cc54b752c396a33
Diagne Axel <axel.diagne2021@campus-eni.fr>
2021-07-21 22:07:05 +0200

    Wish controller, repository et affichage des infos BDD

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,20 +13,24 @@ class WishController extends AbstractController
     /**
      * @Route("/wish/list", name="wish_list")
      */
-    public function list(): Response
+    public function list(WishRepository $wishRepository): Response
     {
-        return $this->render('wish/list.html.twig', [
 
+        $wishList = $wishRepository->findBy([], ['id' => 'ASC']);
+
+        return $this->render('wish/list.html.twig', [
+            "wish" => $wishList
         ]);
     }
 
     /**
      * @Route("/wish/list/detail/{id}", name="wish_detail")
      */
-    public function details(int $id): Response
+    public function details(int $id, WishRepository $wishRepository): Response
     {
+        $detail = $wishRepository->find($id);
         return $this->render('wish/details.html.twig', [
-
+            "detail" => $detail
         ]);
     }
 }
